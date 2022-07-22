4b7586e52b4edeb723e64dad99941a2fceab1df6
David Pocsai <4122dpocsai@gmail.com>
2022-07-22 15:10:59 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>HELLO WORLD!</div>
+      <Routes>
+        <Route exact path="/"></Route>
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/reducers/reducer1.js
@@ -0,0 +1,26 @@
+const reducer1 = (tournaments = [], action) => {
+  switch (action.type) {
+    case "GET_TOURNAMENTS":
+      return action.payload;
+
+    case "GET_TOURNAMENT":
+      return [...tournaments];
+
+    case "CREATE_TOURNAMENT":
+      return [...tournaments, action.payload];
+
+    case "UPDATE_TOURNAMENT":
+      return tournaments.map((tournament) =>
+        tournament._id === action.payload._id ? action.payload : tournament
+      );
+
+    case "DELETE_TOURNAMENT":
+      return tournaments.filter(
+        (tournament) => tournament._id !== action.payload
+      );
+
+    default:
+      return tournaments;
+  }
+};
+export default reducer1;
